7f89c6fde02a34672042f1ea2a9957b01a58ba18
jeremu2907 <jeremu2907@gmail.com>
2024-12-10 23:42:05 -0600

    date

--- client/src/section/Summary.tsx
+++ client/src/section/Summary.tsx
@@ -8,7 +8,7 @@ const Summary: React.FC = () => {
         <section className='flex-[2] bg-baseDark w-full p-10'>
             <h1>Summary</h1>
             <div className='flex justify-evenly mt-12 mb-12'>
-                <Input type='month' onChange={() => {console.log("hi")}} />
+                <Input type='month' onChange={() => {console.log("hi")}} value='2024-12'/>
                 <Button value='.csv' onClick={() => {}} />
             </div>
             <ul className='border-l h-auto w-auto max-h-[60vh] overflow-y-scroll pl-6 pr-6'>
