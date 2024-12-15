979c0c6acb86da6486ccfd09b60bd94ebfca7bfc
jeremu2907 <jeremu2907@gmail.com>
2024-12-15 13:19:41 -0600

    networking

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -3,6 +3,5 @@ import axios from 'axios';
 export const DEV = import.meta.env.DEV
 
 export const api = axios.create({
-    // baseURL: DEV ? "http://localhost:8080/date-entry" : "https://timetrackerserver.jeremyng.dev"
-    baseURL: "http://localhost:8080/date-entry"
+    baseURL: DEV ? "http://localhost:8080/date-entry" : "https://timetrackerserver:8080/date-entry"
 });
