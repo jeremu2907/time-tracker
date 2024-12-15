b0a1e476d28eaf3799dbc350bd3639f69745cf12
jeremu2907 <jeremu2907@gmail.com>
2024-12-15 09:36:21 -0600

    test url

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -3,5 +3,6 @@ import axios from 'axios';
 export const DEV = import.meta.env.DEV
 
 export const api = axios.create({
-    baseURL: DEV ? "http://localhost:8080/date-entry" : "https://timetrackerserver.jeremyng.dev"
+    // baseURL: DEV ? "http://localhost:8080/date-entry" : "https://timetrackerserver.jeremyng.dev"
+    baseURL: "http://localhost:8080/date-entry"
 });
