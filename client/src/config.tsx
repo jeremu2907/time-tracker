cf7186bf9f5a35fefc5ddb9adb5e2da74f548786
jeremu2907 <jeremu2907@gmail.com>
2024-12-15 21:07:13 -0600

    using vpn ip

--- client/src/config.tsx
+++ client/src/config.tsx
@@ -4,5 +4,5 @@ import axios from 'axios';
 
 export const api = axios.create({
     // baseURL: DEV ? "http://localhost:8080/date-entry" : "https://timetrackerserver:8080/date-entry"
-    baseURL: "http://localhost:8080/date-entry"
+    baseURL: "http://10.0.0.2:8080/date-entry"
 });
