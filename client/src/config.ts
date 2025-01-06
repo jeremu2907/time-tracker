7a7775f63b95a6dd5df1696179e3367b515bf3db
jeremu2907 <jeremu2907@gmail.com>
2025-01-05 21:43:15 -0600

    fixed baseURL

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const DEV = import.meta.env.DEV
 
 export const api = axios.create({
-    baseURL: DEV ? "http://localhost:8080/date-entry" : "https://timetrackerserver:8080/date-entry"
+    baseURL: DEV ? "http://localhost:8080/date-entry" : "http://10.0.0.2:8080/date-entry"
 });
